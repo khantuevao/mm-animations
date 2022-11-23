52e6da44f04df68e36541eb304fca7f4ed4e2aba
khantuevao <khantuevao@gmail.com>
2022-11-23 16:03:44 +0600

    Add animation 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
       'silver': '#ecebff',
       'bubble-gum': '#ff77e9',
       'bermuda': '#78dcca',
+      'green': '#84cc16',
       'hider': '#AD3FBF',
       'guesser': '#3FA0BF',
       'basecolor': '#D9D9D9',
@@ -23,14 +24,23 @@ module.exports = {
     extend: {
       keyframes: {
         botToBot: {
-          '0%': { transform: 'translateY(5rem)', opacity: '0' },
+          '0%': { transform: 'translateY(4rem)', opacity: '0' },
           '10%': { transform: 'translateY(0rem)', opacity: '1' },
           '90%': { transform: 'translateY(0rem)', opacity: '1' },
-          '100%': { transform: 'translateY(5rem)', opacity: '0' },
+          '100%': { transform: 'translateY(4rem)', opacity: '0' },
+        },
+        smallToBig: {
+          '0%': { transform: 'scale(0)', opacity: '0' },
+          '15%': { transform: 'scale(1.2)', opacity: '1' },
+          '20%': { transform: 'scale(1)', opacity: '1' },
+          '80%': { transform: 'scale(1)', opacity: '1' },
+          '85%': { transform: 'scale(1.2)', opacity: '1' },
+          '100%': { transform: 'scale(0)', opacity: '0' },
         },
       },
       animation: {
         'bot-to-bot': 'botToBot 4s ease-in-out infinite',
+        'small-to-big': 'smallToBig 4s ease-in-out infinite',
       },
     },
   },
